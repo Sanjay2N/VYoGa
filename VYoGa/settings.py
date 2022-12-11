e10cf3e92df3f9f9d1d4966cb7c8c01609d5f3a8
Sanjay K C <sanjaykcbcs@gmail.com>
2022-12-12 00:10:14 +0530

    Deploying To Railway

--- VYoGa/settings.py
+++ VYoGa/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--3kp@4oxft2c_3l(wz6dyn-t#os3(j$sv(f@b&rjk$8kve!hcn"
+SECRET_KEY = config("SECRET_KEY")
+DEBUG=config("DEBUG",cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 ALLOWED_HOSTS = ["*"]
 
